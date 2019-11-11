f05ff72aef0ccdcf814a1df9bfe747347fd7caa1
François <fxpar33@gmail.com>
2019-11-11 18:09:41 +0100

    corrected auto with browser detection

--- background.js
+++ background.js
@@ -2,16 +2,22 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   console.log('Ecrire la date du lendemain');
-  window.alert(localStorage.getItem("lg"));
+  var language = window.navigator.userLanguage || window.navigator.language;
+  //window.alert(localStorage.getItem("lg"));
+  if ((localStorage.getItem("lg")=="auto")||(localStorage.getItem("lg")=="")){
+	  var lg = language;
+  }else{
+	  var lg = localStorage.getItem("lg")
+  }
   var myCode = `var auj = new Date();
   //délai pour chaque jour, commence à dimanche
   var délai = [1, 1, 1, 1, 1,3, 2]; 
   var livraison = new Date();
     //détecter le language du navigateur pour savoir comment imprimer la date: 20/09/2019 ou 09/20/2019
-  var language = window.navigator.userLanguage || window.navigator.language;
+  
   livraison.setDate(auj.getDate()+délai[auj.getDay()]);
 	if ((document.activeElement.type == "text")||(document.activeElement.type == "textarea")){
-  document.activeElement.value = livraison.toLocaleDateString("`+localStorage.getItem('lg')+`")+document.activeElement.value;
+  document.activeElement.value = livraison.toLocaleDateString("`+lg+`")+document.activeElement.value;
   }`;
  
   chrome.tabs.executeScript({
